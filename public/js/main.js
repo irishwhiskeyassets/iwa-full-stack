4531092755de4496cc930cddfe5509d91b724dae
Conor Deegan <conordeegan@Conors-MacBook-Pro.local>
2019-06-11 20:05:03 +0100

    a really really simple after thought of adding a photo to the form

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,26 @@
 //Hamburger functionality
 $(document).ready(function () {
-        $(".hamburger-container").click(function () {
-            $(".hamburger").toggleClass("is-active");
-            $(".mobile-menu").toggleClass("menu-active");
-        });
+    $(".hamburger-container").click(function () {
+        $(".hamburger").toggleClass("is-active");
+        $(".mobile-menu").toggleClass("menu-active");
     });
-    
+});
 
-    $(document).ready(function () {
-        $('.carousel-container').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: true,
-            dots:true,
-            adaptiveHeight: true
-        });
+
+$(document).ready(function () {
+    $('.carousel-container').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+        adaptiveHeight: true
     });
-    
+});
+
 
 // Radio buttons clicked
 
-function radioClicked(event){
+function radioClicked(event) {
     var bundleClicked = event.currentTarget.id;
     switch (bundleClicked) {
         case 'bronze':
@@ -54,7 +54,7 @@ function radioClicked(event){
             $("#" + bundleClicked + 'Checkmark').addClass("selectedCheckmark");
             $("#" + bundleClicked + 'Radio').prop("checked", true);
             break;
-        
+
         case 'platinum':
             $("#" + bundleClicked).addClass("selectedBundle");
             $("#" + bundleClicked).siblings().removeClass("selectedBundle");
@@ -65,13 +65,34 @@ function radioClicked(event){
     }
 }
 
+// Submit Form
+
 submitButton.addEventListener('click', function (event) {
     var form = document.getElementsByTagName('form');
-
+    $("#imageURL").val(imageURL.imageUrl);
     if (!form[0].checkValidity()) {
         errorMessage.style.display = 'block';
     } else {
         purchaseForm.submit();
-    } 
+    }
 })
 
+// Prevent Default for Image Upload
+
+var imageURL;
+
+$("#cpa-form").submit(function (e) {
+    e.preventDefault();
+    var formData = new FormData();
+    formData.append('image', $('#image')[0].files[0]);
+    $.ajax({
+        url: '/api/form/upload_image',
+        type: 'POST',
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (data) {
+            return imageURL = data;
+        }
+    });
+});
---
+++ services/image-upload.js
@@ -0,0 +1,40 @@
+const aws = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+
+require('dotenv').config()
+
+aws.config.update({
+  secretAccessKey: process.env.AWSSecretKey,
+  accessKeyId: process.env.AWSAccessKeyId,
+  region: "eu-west-1"
+});
+
+const s3 = new aws.S3();
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(new Error('Invalid file type, only JPEG and PNG is allowed!'), false);
+  }
+}
+
+const upload = multer({
+  fileFilter,
+  storage: multerS3({
+    acl: 'public-read',
+    s3: s3,
+    bucket: 'irish-whiskey-test',
+    metadata: function (req, file, cb) {
+      cb(null, {
+        fieldName: 'TESTING_METADATA'
+      });
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString())
+    }
+  })
+});
+
+module.exports = upload;
